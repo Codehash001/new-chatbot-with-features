0d05055d7b5e0a39e1b5dea75339fcdf8deb5f5e
Codehash001 <codehashblockchain@gmail.com>
2023-06-10 19:44:31 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,414 @@
+import { useRef, useState, useEffect, useMemo, useCallback } from 'react';
+import Layout from '@/components/layout';
+import styles from '@/styles/Home.module.css';
+import { Message } from '@/types/chat';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import LoadingDots from '@/components/ui/LoadingDots';
+import { Document } from 'langchain/document';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import Sidebar from '@/components/sidebar';
+import datasetData from '../data.json'
+import Link from 'next/link';
+
+export default function Home() {
+  const [query, setQuery] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [sourceDocs, setSourceDocs] = useState<Document[]>([]);
+  const [allfiles, setallFiles] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [selectedTempFiles, SetSelectedTempFiles] = useState<File[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [messageState, setMessageState] = useState<{
+    messages: Message[];
+    pending?: string;
+    history: [string, string][];
+    pendingSourceDocs?: Document[];
+  }>({
+    messages: [
+      {
+        message: 'Hi, what would you like to learn about this doc?',
+        type: 'apiMessage',
+      },
+    ],
+    history: [],
+    pendingSourceDocs: [],
+  });
+
+  const { messages, pending, history, pendingSourceDocs } = messageState;
+
+  const messageListRef = useRef<HTMLDivElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const [selectedDataset, setSelectedDataset] = useState('');
+
+  const handleDatasetChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    setSelectedDataset(value);
+  };
+  
+  useEffect(() => {
+    textAreaRef.current?.focus();
+  }, []);
+
+  //handle form submission
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+
+    setError(null);
+
+    if (!query) {
+      alert('Please input a question');
+      return;
+    }
+
+    const question = query.trim();
+
+    setMessageState((state) => ({
+      ...state,
+      messages: [
+        ...state.messages,
+        {
+          type: 'userMessage',
+          message: question,
+        },
+      ],
+      pending: undefined,
+    }));
+
+    setLoading(true);
+    setQuery('');
+    setMessageState((state) => ({ ...state, pending: '' }));
+
+    const ctrl = new AbortController();
+
+    try {
+      fetchEventSource('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          question,
+          history,
+          selectedTempFiles,
+        }),
+        signal: ctrl.signal,
+        onmessage: (event) => {
+          if (event.data === '[DONE]') {
+            setMessageState((state) => ({
+              history: [...state.history, [question, state.pending ?? '']],
+              messages: [
+                ...state.messages,
+                {
+                  type: 'apiMessage',
+                  message: state.pending ?? '',
+                  sourceDocs: state.pendingSourceDocs,
+                },
+                
+              ],
+              pending: undefined,
+              pendingSourceDocs: undefined,
+            }));
+            setLoading(false);
+            ctrl.abort();
+          } else {
+            const data = JSON.parse(event.data);
+            if (data.sourceDocs) {
+              setMessageState((state) => ({
+                ...state,
+                pendingSourceDocs: data.sourceDocs,
+              }));
+            } else {
+              setMessageState((state) => ({
+                ...state,
+                pending: (state.pending ?? '') + data.data,
+              }));
+            }
+          }
+        },
+        
+      });
+    } catch (error) {
+      setLoading(false);
+      setError('An error occurred while fetching the data. Please try again.');
+    }
+  }
+
+  //prevent empty submissions
+  const handleEnter = useCallback(
+    (e: any) => {
+      if (e.key === 'Enter' && query) {
+        handleSubmit(e);
+      } else if (e.key == 'Enter') {
+        e.preventDefault();
+      }
+    },
+    [query],
+  );
+
+  const chatMessages = useMemo(() => {
+    return [
+      ...messages,
+      ...(pending
+        ? [
+            {
+              type: 'apiMessage',
+              message: pending,
+              sourceDocs: pendingSourceDocs,
+            },
+          ]
+        : []),
+    ];
+  }, [messages, pending, pendingSourceDocs]);
+
+  //scroll to bottom of chat
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [chatMessages]);
+
+
+  const getFiles = async () => {
+    try {
+      const response = await fetch("/api/upload");
+      const data = await response.json();
+      setallFiles(data.files);
+    } catch (error: any) {
+      console.log(error.response?.data);
+    }
+  };
+
+  const deleteFile = async (fileName: any) => {
+    try {
+      const response = await fetch(`/api/upload?fileName=${fileName}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      if (response.ok) {
+        getFiles()
+      } else {
+        setErrorMessage(data.error);
+      }
+    } catch (error: any) {
+      console.log(error.response?.data);
+    }
+  }
+
+  useEffect(() => {
+    getFiles()
+  }, []);
+
+  const handleCheckboxChange = (file: File) => {
+    if (selectedTempFiles.includes(file)) {
+      SetSelectedTempFiles(selectedTempFiles.filter((item) => item !== file));
+    } else {
+      SetSelectedTempFiles([...selectedTempFiles, file]);
+    }
+  };
+
+  return (
+    <> 
+    <div className='flex flex-row'>
+    <div className=" h-screen w-[400px] bg-slate-200 px-4 py-10 flex flex-col justify-between space-y-5">
+                
+
+            <div>
+            <div className="rounded-md mx-4 bg-black text-white px-4 py-2 mb-2 text-center">Current dataset details</div>
+            <h1 className=" text-center text-black font-bold my-3">Filter from Uploaded Documents</h1>
+            <ul className="text-black px-4 overflow-auto h-full custom-scrollbar">
+        {allfiles.map((file) => (
+          <li className="space-x-6 flex items-center my-2 justify-between border-b-2 border-gray-300 py-2" key={file}>
+            <div className="font-semibold md:text-[16px]">
+            <input
+            type="checkbox"
+            checked={selectedTempFiles.includes(file)}
+            onChange={() => handleCheckboxChange(file)}
+            className="mr-2"
+          />
+          {file}{" "}
+            </div>
+            <button className="" onClick={() => deleteFile(file)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" className="fill-red-600"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path></svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+            </div>
+
+            <div className="flex flex-row justify-center mt-5">
+                <Link href="/ingest">
+                <div className="rounded-md mx-4 bg-blue-400 text-white px-4 py-2 mb-2 hover:bg-blue-500">Ingest more documents</div>
+                </Link>
+            </div>
+        </div>
+      <Layout>
+        <div className="mx-auto flex flex-col gap-4">
+          <div className='w-full flex flex-row justify-center p-4'>
+          <h1 className="text-2xl font-bold leading-[1.1] tracking-wide text-center">
+            QA Chatbot for Custom Docs
+          </h1>
+          </div>
+          <main className={styles.main}>
+            <div className={styles.cloud}>
+              <div ref={messageListRef} className={styles.messagelist}>
+                {chatMessages.map((message, index) => {
+                  let icon;
+                  let className;
+                  if (message.type === 'apiMessage') {
+                    icon = (
+                      <Image
+                        src="/bot-image.png"
+                        alt="AI"
+                        width="40"
+                        height="40"
+                        className={styles.boticon}
+                        priority
+                      />
+                    );
+                    className = styles.apimessage;
+                  } else {
+                    icon = (
+                      <Image
+                        src="/usericon.png"
+                        alt="Me"
+                        width="40"
+                        height="40"
+                        className={styles.usericon}
+                        priority
+                      />
+                    );
+                    // The latest message sent by the user will be animated while waiting for a response
+                    className =
+                      loading && index === chatMessages.length - 1
+                        ? styles.usermessagewaiting
+                        : styles.usermessage;
+                  }
+                  return (
+                    <>
+                      <div key={`chatMessage-${index}`} className={className}>
+                        {icon}
+                        <div className={styles.markdownanswer}>
+                          <ReactMarkdown linkTarget="_blank">
+                            {message.message}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                      {message.sourceDocs && (
+                        <div
+                          className="p-5"
+                          key={`sourceDocsAccordion-${index}`}
+                        >
+                          <Accordion
+                            type="single"
+                            collapsible
+                            className="flex-col"
+                          >
+                            {message.sourceDocs.map((doc, index) => (
+                              <div key={`messageSourceDocs-${index}`}>
+                                <AccordionItem value={`item-${index}`}>
+                                  <AccordionTrigger>
+                                    <h3>Source {index + 1}</h3>
+                                  </AccordionTrigger>
+                                  <AccordionContent>
+                                    <ReactMarkdown linkTarget="_blank">
+                                      {doc.pageContent}
+                                    </ReactMarkdown>
+                                    <p className="mt-2">
+                                      <b>Source:</b> {doc.metadata.source}
+                                    </p>
+                                  </AccordionContent>
+                                </AccordionItem>
+                              </div>
+                            ))}
+                          </Accordion>
+                        </div>
+                      )}
+                    </>
+                  );
+                })}
+                {sourceDocs.length > 0 && (
+                  <div className="p-5">
+                    <Accordion type="single" collapsible className="flex-col">
+                      {sourceDocs.map((doc, index) => (
+                        <div key={`SourceDocs-${index}`}>
+                          <AccordionItem value={`item-${index}`}>
+                            <AccordionTrigger>
+                              <h3>Source {index + 1}</h3>
+                            </AccordionTrigger>
+                            <AccordionContent>
+                              <ReactMarkdown linkTarget="_blank">
+                                {doc.pageContent}
+                              </ReactMarkdown>
+                            </AccordionContent>
+                          </AccordionItem>
+                        </div>
+                      ))}
+                    </Accordion>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className={styles.center}>
+              <div className={styles.cloudform}>
+                <form onSubmit={handleSubmit}>
+                  <textarea
+                    disabled={loading}
+                    onKeyDown={handleEnter}
+                    ref={textAreaRef}
+                    autoFocus={false}
+                    rows={1}
+                    maxLength={512}
+                    id="userInput"
+                    name="userInput"
+                    placeholder={
+                      loading
+                        ? 'Waiting for response...'
+                        : 'What is this doc about?'
+                    }
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className={styles.textarea}
+                  />
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className={styles.generatebutton}
+                  >
+                    {loading ? (
+                      <div className={styles.loadingwheel}>
+                        <LoadingDots color="#000" />
+                      </div>
+                    ) : (
+                      // Send icon SVG in input field
+                      <svg
+                        viewBox="0 0 20 20"
+                        className={styles.svgicon}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </div>
+            {error && (
+              <div className="border border-red-400 rounded-md p-4">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </main>
+        </div>
+      </Layout>
+      </div>
+    </>
+  );
+}
